8713f561962c15d89c31783e84ee0e0b63acc51f
2401_83521672 <2401_83521672@noreply.gitcode.com>
2026-03-10 11:43:25 +0800

    测试文件模版

---
+++ engine/src/test/java/org/pureglx/engine/test.java
@@ -0,0 +1,15 @@
+package org.pureglx.engine;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class test {
+
+    @Test
+    void test(){
+        System.out.println("test");
+    }
+}
