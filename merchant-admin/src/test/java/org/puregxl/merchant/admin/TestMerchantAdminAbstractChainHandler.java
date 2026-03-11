f2dcc75db0be2e572ae45b0ac6cf14d2d31ec98e
2401_83521672 <2401_83521672@noreply.gitcode.com>
2026-03-11 19:24:19 +0800

    feature：开发了分库分表的功能 解决了数据倾斜的问题

--- merchant-admin/src/test/java/org/puregxl/merchant/admin/TestMerchantAdminAbstractChainHandler.java
+++ merchant-admin/src/test/java/org/puregxl/merchant/admin/TestMerchantAdminAbstractChainHandler.java
@@ -1,6 +1,8 @@
 package org.puregxl.merchant.admin;
 
 
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.RandomUtil;
 import org.junit.jupiter.api.Test;
 import org.puregxl.merchant.admin.dao.entity.CouponTemplateDO;
 import org.puregxl.merchant.admin.dao.mapper.CouponTemplateMapper;
@@ -8,16 +10,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
-
 public class TestMerchantAdminAbstractChainHandler {
 
     @Autowired
-    private  CouponTemplateMapper couponTemplateMapper;
+    private CouponTemplateMapper couponTemplateMapper;
 
-    @Test
-    public void test(){
+    private final ExecutorService executorService = new ThreadPoolExecutor(
+            10,
+            10,
+            9999,
+            TimeUnit.SECONDS,
+            new SynchronousQueue<>(),
+            new ThreadPoolExecutor.CallerRunsPolicy()
+    );
+
+    private static final int maxCount = 5000;
+
+    private final List<Snowflake> snowflakes = new ArrayList<>(20);
+
+    public CouponTemplateDO build(){
 
         CouponTemplateDO couponTemplateDO = new CouponTemplateDO();
         couponTemplateDO.setName("新用户满100减20券");
@@ -32,8 +51,30 @@ public class TestMerchantAdminAbstractChainHandler {
         couponTemplateDO.setReceiveRule("{\"limitPerUser\":1,\"receiveStartTime\":\"2026-03-10 00:00:00\",\"receiveEndTime\":\"2026-03-31 23:59:59\"}");
         couponTemplateDO.setConsumeRule("{\"minimumAmount\":100,\"discountAmount\":20,\"canStack\":false}");
         couponTemplateDO.setStatus(0);
-
-        couponTemplateMapper.insert(couponTemplateDO);
-
+        return couponTemplateDO;
     }
+
+
+    public void beforeData() {
+        for (int i = 0 ; i < 20 ; i++) {
+            snowflakes.add(new Snowflake(i));
+        }
+    }
+
+    @Test
+    public void testShardingSphere() throws InterruptedException {
+        snowflakes.clear();
+        beforeData();
+        for (int i  = 0 ; i < maxCount ; i++) {
+            executorService.execute(() -> {
+                CouponTemplateDO build = build();
+                build.setShopNumber(snowflakes.get(RandomUtil.randomInt(20)).nextId());
+                couponTemplateMapper.insert(build);
+            });
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(10, TimeUnit.MINUTES);
+    }
+
 }
